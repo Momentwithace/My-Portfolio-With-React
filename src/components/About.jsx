cb6f0e94f4246ccb782797c12b75d6d7dfbc2ac2
momemtwithace <augustineezekiel763@gmail.com>
2022-11-18 13:59:42 +0100

    My portfolio first project

---
+++ src/components/About.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" 
+        className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+
+        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+
+            <div className="pb-8">
+                <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+                    About
+                </p>
+            </div>
+            <p className="text-xl mt-20">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Praesentium tempora nesciunt ut cum iste repellat autem molestiae 
+                molestias, dolorem ipsam non nostrum excepturi error aspernatur 
+                voluptatibus animi ea inventore odio provident amet similique 
+                corporis sint necessitatibus explicabo! Placeat temporibus libero
+                neque ad iusto laborum modi exercitationem illum eos incidunt vero
+                eveniet beatae in odio hic facilis omnis, reiciendis qui asperiores 
+                optio tempore quam dolores sed autem. Nihil aut laudantium, doloribus 
+                  
+            </p>
+
+            <br/>
+
+            <p className="text-xl mt-20">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Praesentium tempora nesciunt ut cum iste repellat autem molestiae 
+                molestias, dolorem ipsam non nostrum excepturi error aspernatur 
+                voluptatibus animi ea inventore odio provident amet similique 
+                corporis sint necessitatibus explicabo! Placeat temporibus libero
+                neque ad iusto laborum modi exercitationem illum eos incidunt vero
+                eveniet beatae in odio hic facilis omnis, reiciendis qui asperiores 
+                optio tempore quam dolores sed autem. Nihil aut laudantium, doloribus 
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,90 @@
+import React from 'react'
+import arrayDestruct from "../assert/portfolio/arrayDestruct.jpg";
+import installNode from "../assert/portfolio/installNode.jpg";
+import navBar from "../assert/portfolio/navbar.jpg";
+import reactParallax from "../assert/portfolio/reactParallax.jpg";
+import reactSmooth from "../assert/portfolio/reactSmooth.jpg";
+import reactWeather from "../assert/portfolio/reactWeather.jpg";
+
+
+const Portfolio = () => {
+
+    const portfolios = [
+        {
+            id:1,
+            src: arrayDestruct
+        },
+
+        {
+            id:2,
+            src:installNode
+        },
+
+        {
+            id:1,
+            src: navBar
+        },
+
+        {
+            id:1,
+            src: reactParallax
+        },
+
+        {
+            id:1,
+            src: reactSmooth
+        },
+
+        {
+            id:1,
+            src: reactWeather
+        }
+    ]
+
+  return <div name="portfolio" className="bg-gradient-to-b from-black to-gray-800 w-full
+   text-white md:h-screen">
+
+    <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+
+        <div className="pb-8">
+            <p className="text-4xl font-bold inline border-b-4 border-gray-500"
+            >Portfolio</p>
+
+            <p className="py-6"
+            >Check out my work right here</p>
+        </div>
+
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+
+
+        {portfolios.map(({id, src}) => (
+
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+                <img  
+                src={src} 
+                alt=""
+                className="rounded-md duration-200 hover:scale-105"
+                />
+            <div className="flex items-center justify-center">
+
+            <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
+            >Demo
+            </button>
+
+            <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
+            >Code
+            </button>
+            </div>
+
+            </div>
+
+        ))}
+
+        </div>
+
+    </div>
+  </div>
+}
+
+export default Portfolio
