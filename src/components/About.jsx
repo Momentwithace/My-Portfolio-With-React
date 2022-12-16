fa7a83bd74ab0c181bd3c88acfb825e2218ce9aa
momemtwithace <augustineezekiel763@gmail.com>
2022-12-16 18:22:53 +0100

    New commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 const About = () => {
   return (
     <div name="about" 
-        className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+        className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white '>
 
-        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full mt-">
 
             <div className="pb-8">
                 <p className="text-4xl font-bold inline border-b-4 border-gray-500">
                     About Me
                 </p>
             </div>
-            <p className="text-xl mt-20">
+            <p className="text-xl mt-">
                 My name's Ace, I'm 28, currently living in Lagos State, West-Africa Nigeria.
                 I'm a Software Engineer, prior to my journey into Software Engineering, I own
                 own a Business of my own here in Lagos, That's been on for years.
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ import { Link } from 'react-scroll'
 const Home = () => {
   return (
     <div name="home" className="h-screen w-full bg-gradient-to-b from-black
-     via-black  to-gray-800">
+     via-black  to-gray-800 ">
 
      <div className="max-w-screen-lg mx-auto flex flex-col items-center
       justify-center h-full px-4 md:flex-row  ">
@@ -27,17 +27,14 @@ const Home = () => {
             I've worked with great technologies, like Java, Python, Mysql, PosTgresl alongside frameworks
             like Sprinboot, Django, FastApi. 
             </p>
-            <p  className="text-gray-500 py-4 max-w-md" >
-            I have little knowledge of Mobile Development, working with Koltin and Flutter.
-            </p>
-
+           
             <div>
                 <Link to="portfolio"
                 smooth
                 duration={500}
                 className="group text-white w-fit px-6 py-3 my-2
                 flex items-center rounded-md bg-gradient-to-r from-cyan-500
-                to-blue-50o cursor-pointer"
+                to-blue-50o cursor-pointer mb-20"
                 >
                     Portfolio
                     <span className="group-hover:rotate-90 duration-300">
